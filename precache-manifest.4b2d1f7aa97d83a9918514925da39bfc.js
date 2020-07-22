991e71345e4d4aa681fd38e5ab8b65f2b06e45f7
austen cote <coteausten@gmail.com>
2020-07-22 15:18:00 -0700

    Updates

---
+++ precache-manifest.4b2d1f7aa97d83a9918514925da39bfc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fa8b16ff02696bac55c2dff75c618c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "020c67f76dfc0ad02ee0",
+    "url": "/Portfolio/static/css/2.f3fc1a0a.chunk.css"
+  },
+  {
+    "revision": "fac7f1ffd2fe9a1fdeca",
+    "url": "/Portfolio/static/css/main.36ff785b.chunk.css"
+  },
+  {
+    "revision": "020c67f76dfc0ad02ee0",
+    "url": "/Portfolio/static/js/2.ee83c6ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.ee83c6ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac7f1ffd2fe9a1fdeca",
+    "url": "/Portfolio/static/js/main.07c35d1f.chunk.js"
+  },
+  {
+    "revision": "3e5bdc5906b563142154",
+    "url": "/Portfolio/static/js/runtime-main.b3fdf137.js"
+  },
+  {
+    "revision": "6d8b8d32e4b00489163a334a9e62568a",
+    "url": "/Portfolio/static/media/Chat.6d8b8d32.PNG"
+  },
+  {
+    "revision": "d27a99870686f015ac5c20f176af20d9",
+    "url": "/Portfolio/static/media/FFTG.d27a9987.PNG"
+  },
+  {
+    "revision": "54d27513f75220a46005825d13dbb902",
+    "url": "/Portfolio/static/media/HOMR.54d27513.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "def5005f6dd5f5cdc717b13e27738a68",
+    "url": "/Portfolio/static/media/personal.def5005f.jpg"
+  },
+  {
+    "revision": "1122b60321a8b3772bc32b04b9ffd945",
+    "url": "/Portfolio/static/media/smith.1122b603.jpg"
+  },
+  {
+    "revision": "97bd2b30efb8fec25b00c3f740dfa5a5",
+    "url": "/Portfolio/static/media/tetons.97bd2b30.JPG"
+  }
+]);
